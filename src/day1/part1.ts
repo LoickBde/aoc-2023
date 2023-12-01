ff867f01103be8b7d11cb1f09396232ecb703db8
LoickBde <loick.baude@decathlon.com>
2023-12-01 11:04:24 +0100

    day 1 part 1

--- src/day1/part1.ts
+++ src/day1/part1.ts
@@ -1,1 +1,22 @@
 import * as fs from "fs";
+
+const startTime = performance.now();
+
+const filePath = `${__dirname}/input.txt`;
+const rawFileContent = fs.readFileSync(filePath, "utf-8");
+const fileContent = rawFileContent.split("\n");
+
+const result = fileContent.reduce((result, current) => {
+  const onlyDigits = current.replace(/\D/g, "");
+  const stringfiedValue =
+    onlyDigits?.length >= 2
+      ? `${onlyDigits[0]}${onlyDigits[onlyDigits.length - 1]}`
+      : `${onlyDigits[0]}${onlyDigits[0]}`;
+  const value = +stringfiedValue;
+
+  return result + value;
+}, 0);
+
+const time = performance.now() - startTime;
+
+console.log(`Result: ${result}. Ran day 1 part 1 in ${time} ms.`);
